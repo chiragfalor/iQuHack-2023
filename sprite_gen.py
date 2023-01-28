39be1af8ace52febf6c14d3bf45f0aa216d0e51e
chiragfalor <67641147+chiragfalor@users.noreply.github.com>
2023-01-28 15:28:20 -0500

    Sprite gen with random walk

--- sprite_gen.py
+++ sprite_gen.py
@@ -8,10 +8,30 @@ import random
 def bits_to_int(bits):
     return sum([bit * 2 ** i for i, bit in enumerate(bits)])
 
+class RandomWalker:
+
+    def __init__(self, walk_space, start_point=None):
+        if start_point is None:
+            start_point = len(walk_space) // 2
+        self.walk_space = walk_space
+        self.current_point = start_point
+
+    def step(self, direction_bit):
+        if self.current_point == 0:
+            self.current_point = 1
+        elif self.current_point == len(self.walk_space) - 1:
+            self.current_point = len(self.walk_space) - 2
+        else:
+            self.current_point += (1 if direction_bit == 1 else -1)
+
+        return self.walk_space[self.current_point]
+
+            
+
 class Sprite:
 
-    BITS_PER_COLOR_COMPONENT = 3
-    NUM_COLORS = 2
+    BITS_PER_COLOR_COMPONENT = 2
+    NUM_COLORS = 3
     BITS_PER_LOCATION = 2 # should be 1 + log2(NUM_COLORS)
     PIXEL_SIZE = 64
 
@@ -25,7 +45,7 @@ class Sprite:
     def get_random_color(self, random_bits):
         assert len(random_bits) == self.BITS_PER_COLOR_COMPONENT * 3
         def get_color_component(bits):
-            MIN_VAL = 64
+            MIN_VAL = 32
             MAX_VAL = 255
             return int(MIN_VAL + (MAX_VAL - MIN_VAL) * bits_to_int(bits) / (2 ** self.BITS_PER_COLOR_COMPONENT - 1))
         return (get_color_component(random_bits[0:3]), get_color_component(random_bits[3:6]), get_color_component(random_bits[6:9]))
@@ -37,22 +57,41 @@ class Sprite:
         # the array is symmetric about the vertical axis
         element_array = [[0 for _ in range(self.dims[1])] for _ in range(self.dims[0])]
 
-        def get_element(bits):
-            return 0 if bits[0] == 0 else ((bits_to_int(bits[1:]) % self.NUM_COLORS) + 1)
-
+        self.random_walker = None
 
         for i in range(self.dims[0]//2 + 1):
             for j in range(self.dims[1]):
                 bits = random_bits[(i * self.dims[1] + j) * self.BITS_PER_LOCATION:(i * self.dims[1] + j + 1) * self.BITS_PER_LOCATION]
-                element_array[i][j] = get_element(bits)
+                element_array[i][j] = self.get_element_random_walk(bits)
                 element_array[self.dims[0] - i - 1][j] = element_array[i][j]
 
         return element_array
 
+    
+    def get_element(self, bits):
+        return 0 if bits[0] == 0 else ((bits_to_int(bits[1:]) % self.NUM_COLORS) + 1)
 
-    def get_color_palette(self, random_bits):
+    def get_element_random_walk(self, bits):
+        # Needs only one bit, fix later
+        if self.random_walker is None:
+            self.random_walker = RandomWalker([i for i in range(1, self.NUM_COLORS + 1)])
+
+        element = self.random_walker.step(bits[1])
+        
+        if bits[0] == 0:
+            return 0
+        else:
+            return element
+
+
+
+    def get_color_palette(self, random_bits, gradient_size=0):
         assert len(random_bits) == self.NUM_COLORS * self.BITS_PER_COLOR_COMPONENT * 3
         color_palette = [self.get_random_color(random_bits[i * self.BITS_PER_COLOR_COMPONENT * 3:(i + 1) * self.BITS_PER_COLOR_COMPONENT * 3]) for i in range(self.NUM_COLORS)]
+        
+        if gradient_size > 0:
+            color_palette = [color_palette[0]] + [tuple([int(color_palette[0][i] + (color_palette[1][i] - color_palette[0][i]) * j / gradient_size) for i in range(3)]) for j in range(1, gradient_size + 1)] + [color_palette[1]]
+        
         color_palette.insert(0, (0, 0, 0))
         return color_palette
 
@@ -64,7 +103,7 @@ class Sprite:
         self.color_randomness = self.random_array[:self.NUM_COLORS * self.BITS_PER_COLOR_COMPONENT * 3]
         self.location_randomness = self.random_array[self.NUM_COLORS * self.BITS_PER_COLOR_COMPONENT * 3: self.NUM_COLORS * self.BITS_PER_COLOR_COMPONENT * 3 + self.BITS_PER_LOCATION * ((self.dims[0] // 2) + 1) * self.dims[1]]
 
-        self.color_palette = self.get_color_palette(self.color_randomness)
+        self.color_palette = self.get_color_palette(self.color_randomness, gradient_size=0)
         self.image_elements = self.get_image_elements(self.location_randomness)
 
         for i in range(self.dims[0]):
@@ -98,9 +137,10 @@ class Sprite:
 
 if __name__ == "__main__":
     for i in range(50):
-        random_array = [random.randint(0, 1) for _ in range(50)]
+        random_array = [random.randint(0, 1) for _ in range(60)]
         sprite = Sprite(random_array)
-        sprite.save(f"sprites/test_{i}.png")
+        sprite.save(f"sprites_random_walk/sprite_{i}.png")
+        print(f"Saved sprite {i}")
     
 
     
